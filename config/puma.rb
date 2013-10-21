e5f113f9a8c664142267865e3bce14386f5188b4
miraks <a@vldkn.net>
2013-10-21 23:58:34 +0400

    puma config

---
+++ config/puma.rb
@@ -0,0 +1,24 @@
+app_root = ENV['BOX_PATH']
+
+config = {
+  min_threads_count: ENV['BOX_MIN_THREADS_COUNT'],
+  max_threads_count: ENV['BOX_MAX_THREADS_COUNT'],
+  workers_count: ENV['BOX_WORKERS_COUNT'],
+  env: ENV['BOX_ENV']
+}
+
+threads config.fetch(:min_threads_count, 4).to_i, config.fetch(:max_threads_count).to_i
+workers config.fetch(:workers_count, 4).to_i
+bind 'unix:///tmp/puma.box.sock'
+pidfile File.join(app_root, 'tmp/pids/puma.pid')
+state_path File.join(app_root, 'tmp/pids/puma.state')
+activate_control_app 'unix:///tmp/pumactl.sock'
+environment config.fetch(:env, 'production')
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
+
+preload_app!
