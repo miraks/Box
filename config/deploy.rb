83c0ac5b875e88278a327d37c4711365ae1b6d33
miraks <a@vldkn.net>
2013-10-23 15:14:28 +0400

    binstubs and capistrano setup

---
+++ config/deploy.rb
@@ -0,0 +1,37 @@
+set :application, 'box'
+set :repo_url, 'git@bitbucket.org:miraks/box.git'
+
+set :branch, :master
+
+set :deploy_to, '/var/www/box'
+set :scm, :git
+
+set :format, :pretty
+set :log_level, :debug
+
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+set :keep_releases, 10
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+
+end
