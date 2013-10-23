6d9e623aac111b1518ad3bb81e3e2b3383c682b4
miraks <a@vldkn.net>
2013-10-23 11:24:57 +0400

    удалил коммент в настройках bullet

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -4,5 +4,4 @@ if Rails.configuration.x.bullet.enable
   Bullet.bullet_logger = true
   Bullet.console = true
   Bullet.rails_logger = true
-  #Bullet.add_footer = true #undefined method `add_footer=' for Bullet:Module
 end
