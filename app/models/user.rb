dcc644162a0c74b01ce9c6f1ec118c862fefacc3
miraks <a@vldkn.net>
2013-10-24 23:33:19 +0400

    настроил friendly_id для юзеров

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
+  extend FriendlyId
+
+  friendly_id :name
+
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: true
 
   # TODO: over thousand lines
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131022214624) do
+ActiveRecord::Schema.define(version: 20131024192537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,9 @@ ActiveRecord::Schema.define(version: 20131022214624) do
     t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
+
+  add_index "users", ["slug"], name: "index_users_on_slug", unique: true, using: :btree
 
 end
