20a17419d81e3f5244997c691b8c1b6d070d6810
miraks <a@vldkn.net>
2013-11-30 21:24:42 +0400

    permissions check fix

--- app/models/concerns/lockable.rb
+++ app/models/concerns/lockable.rb
@@ -7,7 +7,7 @@ module Lockable
 
   def permission user
     case
-    when self.user == user || !password? && user.has_access?(self) then :yes
+    when self.user == user || (!password? && user.has_access?(self)) then :yes
     when password? then :password
     else :no
     end
