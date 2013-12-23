68d8371dd5f2a7c0c37ac4696c24d0532cac7216
miraks <a@vldkn.net>
2013-12-23 22:03:42 +0400

    gems update and temp hack for Oj.mimic_json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,19 +11,6 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  # Hack until fix of
-  # https://github.com/rubysl/rubysl-date/issues/3
-  def current_user
-    retried = false
-    begin
-      super
-    rescue => e
-      return raise if retried
-      retried = true
-      retry
-    end
-  end
-
   def after_sign_in_path_for resource
     user_path resource
   end
@@ -32,6 +19,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :is_company <<
-      { company_data: [:name, :activities, :site_link, :address, :phone_number] }
+      { company_data: User::COMPANY_DATA_FIELDS }
   end
 end
